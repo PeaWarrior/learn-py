81b78e8d991d84f26ac9cf6d207c3ec50e9361d4
PeaWarrior <jacksonchen171@gmail.com>
2021-02-06 22:41:12 -0500

    add ex_09

---
+++ ex_09/ex_09_03.py
@@ -0,0 +1,7 @@
+# Exercise 3: Add code to the above program to figure out who has the most messages in the file. After all the data has been read and the dictionary has been created, look through the dictionary using a maximum loop (see Chapter 5: Maximum and minimum loops) to find who has the most messages and print how many messages the person has.
+
+# Enter a file name: mbox-short.txt
+# cwen@iupui.edu 5
+
+# Enter a file name: mbox.txt
+# zqian@umich.edu 195
