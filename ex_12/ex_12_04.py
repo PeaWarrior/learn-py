56acc5d511da3c8fa0914f4e406e7a5b4605cf81
PeaWarrior <jacksonchen171@gmail.com>
2021-02-08 15:09:14 -0500

    change soup.find_all to soup(p)

--- ex_12/ex_12_04.py
+++ ex_12/ex_12_04.py
@@ -3,9 +3,15 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import ssl
+
+# Ignore SSL certificate errors
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
 
 url = input('Enter URL: ')
-html = urlopen(url)
+html = urlopen(url, context=ctx)
 
 soup = BeautifulSoup(html, 'html.parser')
-print('Number of p tags:', len(soup.find_all('p')))
+print('Number of p tags:', len(soup('p')))
