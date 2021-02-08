3463e0c8f1d1eb72479bd9a37a778a6570e4aac4
PeaWarrior <jacksonchen171@gmail.com>
2021-02-07 20:24:48 -0500

    add ex_11

---
+++ ex_11_02.py
@@ -0,0 +1,10 @@
+# Exercise 2: Write a program to look for lines of the form:
+
+# New Revision: 39772
+# Extract the number from each of the lines using a regular expression and the findall() method. Compute the average of the numbers and print out the average as an integer.
+
+# Enter file:mbox.txt
+# 38549
+
+# Enter file:mbox-short.txt
+# 39756
