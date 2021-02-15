64de04dd33a512116729c8f38ac1cadb8f698792
PeaWarrior <jacksonchen171@gmail.com>
2021-02-15 13:53:14 -0500

    add change_slot

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -1,15 +1,22 @@
-row1 = [' ', ' ', ' ']
-row2 = [' ', ' ', ' ']
-row3 = [' ', ' ', ' ']
+import math
 
-def display(row1, row2, row3):
-  print(row1)
-  print(row2)
-  print(row3)
+rows= [
+  [' ', ' ', ' '],
+  [' ', ' ', ' '],
+  [' ', ' ', ' ']
+]
+clear = "\n" * 100
+game_on = True
+user = 'X'
+
+def display(rows):
+  print(rows[0])
+  print(rows[1])
+  print(rows[2])
 
 def get_user_input():
   user_input = ' '
-  acceptable_range = range(1,9)
+  acceptable_range = range(1,10)
   within_range = False
 
   while user_input.isdigit() == False or within_range == False:
@@ -21,9 +28,41 @@ def get_user_input():
       if int(user_input) in acceptable_range:
         within_range = True
       else:
-        print('Sorry, number must be 1 - 9. Try again!')
+        print('Sorry, number out of acceptable range (1 - 9). Try again!')
 
-  return int(user_input)
+  return int(user_input) - 1
 
-display(row1, row2, row3)
-get_user_input()
+def start_game():
+  user_start = 'wrong'
+  while user_start not in ['Y', 'N']:
+    user_start = input('Would you like to keep playing? Y or N ').upper()
+    if user_start not in ['Y', 'N']:
+      print(clear)
+      print('Please choose Y or N')
+  
+  if user_start == 'Y':
+    return True
+  else:
+    return False
+  
+def change_slot():
+  valid_input = False
+
+  while valid_input == False:
+    user_input = get_user_input()
+    row = math.floor(user_input / 3)
+    slot = user_input % 3
+  
+    if rows[row][slot] == ' ':
+      rows[row][slot] = user
+      valid_input = True
+    else:
+      print('Slot already taken. Choose a different slot (1 - 9).')
+
+
+while game_on:
+  display(rows)
+  change_slot()
+  user = 'O' if user == 'X' else 'X'
+
+  game_on = start_game()
